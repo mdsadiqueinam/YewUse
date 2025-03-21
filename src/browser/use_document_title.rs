de44330dc565e77c4a008b7eb01d161b08dbe09f
Md Sadique Inam <md.sadique32@gmail.com>
2025-03-21 16:01:25 +0530

    fix: Refactor use_document_title hook for improved title setting logic

--- src/browser/use_document_title.rs
+++ src/browser/use_document_title.rs
@@ -1,4 +1,3 @@
-use wasm_bindgen::JsCast;
 use web_sys::window;
 use yew::prelude::*;
 
@@ -12,7 +11,7 @@ use yew::prelude::*;
 ///
 /// ```rust
 /// use yewuse::browser::use_document_title;
-/// 
+///
 /// #[function_component(MyComponent)]
 /// fn my_component() -> Html {
 ///     use_document_title("My Page Title".to_string());
@@ -21,15 +20,10 @@ use yew::prelude::*;
 /// ```
 #[hook]
 pub fn use_document_title(title: String) {
-    use_effect_with_deps(
-        move |title| {
-            if let Some(window) = window() {
-                if let Some(document) = window.document() {
-                    document.set_title(title);
-                }
-            }
-            || {}
-        },
-        title,
-    );
+    use_effect_with(title, move |title| {
+        if let Some(document) = window().and_then(|w| w.document()) {
+            document.set_title(title);
+        }
+        || {}
+    });
 }
